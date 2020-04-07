9537727e1405323b4d5b1038439b1addba885596
Takuya Ueda <uedatakuya@gmail.com>
2020-04-08 01:43:45 +0900

    Add test cases

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -18,14 +18,19 @@ func f() {
 	time.Sleep(5 * time.Second) // OK
 
 	const i = 6
-	time.Sleep(i)                                // want `must not use untyped constant as a time\.Duration type`
-	time.Sleep(7)                                // want `must not use untyped constant as a time\.Duration type`
-	time.Sleep(time.Duration(3600))              // OK
-	time.Sleep(time.Duration(5) * time.Second)   // OK
-	time.Sleep(60 * 60)                          // want `must not use untyped constant as a time\.Duration type`
-	time.Sleep(i * 60)                           // want `must not use untyped constant as a time\.Duration type`
+	time.Sleep(i)                              // want `must not use untyped constant as a time\.Duration type`
+	time.Sleep(7)                              // want `must not use untyped constant as a time\.Duration type`
+	time.Sleep(time.Duration(3600))            // OK
+	time.Sleep(time.Duration(5) * time.Second) // OK
+	time.Sleep(60 * 60)                        // want `must not use untyped constant as a time\.Duration type`
+	time.Sleep(i * 60)                         // want `must not use untyped constant as a time\.Duration type`
+
+	const c = 2i * 2i
+	time.Sleep(10 + c) // want `must not use untyped constant as a time\.Duration type`
+
 	context.WithTimeout(context.Background(), i) // want `must not use untyped constant as a time\.Duration type`
-	(T{}).sleep(i)                               // want `must not use untyped constant as a time\.Duration type`
+
+	(T{}).sleep(i) // want `must not use untyped constant as a time\.Duration type`
 }
 
 type T struct{}
